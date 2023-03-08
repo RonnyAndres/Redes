7ec7ee44fb453e61ab3c2d2559d30dc6de694ac7
RonnyAndres <76404004+RonnyAndres@users.noreply.github.com>
2023-03-08 14:10:06 -0500

    Update punt3-lab9.sh

--- punt3-lab9.sh
+++ punt3-lab9.sh
@@ -4,13 +4,22 @@
 #sudo apt-get install vsftpd
 
 # Crear nuevo usuario y establecer su contraseña
-read -p "Ingrese el nombre del nuevo usuario FTP: " nombre_usuario
-sudo adduser $nombre_usuario
-sudo passwd $nombre_usuario
+read -p "Ingrese el nombre del nuevo usuario: " nombre_usuario
+
+# Verificar que el usuario no exista
+if id "$nombre_usuario" >/dev/null 2>&1; then
+    echo "El usuario $nombre_usuario ya existe."
+    exit 1
+fi
+
+# Crear el usuario con su nombre como contraseña
+sudo useradd -m -p $(openssl passwd -1 $nombre_usuario) $nombre_usuario
+
+echo "El usuario $nombre_usuario se ha creado correctamente con su nombre como contraseña."
 
 # Crear lista de usuarios permitidos
 echo "Usuarios disponibles:"
-compgen -u | grep -v '^ftp$'
+compgen -u | grep -v '^ftp$\|^guest\|^nobody\|root'
 usuarios_permitidos=()
 while true; do
     read -p "Ingrese el nombre de usuario permitido (dejar en blanco para terminar): " usuario
@@ -22,21 +31,23 @@ while true; do
         usuarios_permitidos+=($usuario)
         echo "Usuarios agregados hasta el momento: ${usuarios_permitidos[*]}"
         echo "Usuarios disponibles:"
-        comm -23 <(compgen -u | grep -v '^ftp$' | sort) <(echo "${usuarios_permitidos[*]}" | tr ' ' '\n' | sort)
+        comm -23 <(compgen -u | grep -v '^ftp$\|^guest\|^nobody\|root' | sort) <(echo "${usuarios_permitidos[*]}" | tr ' ' '\n' | sort)
     else
         echo "El usuario no existe."
     fi
 done
 echo "${usuarios_permitidos[*]}" | tr ' ' '\n' | sudo tee /etc/vsftpd.user_list
 
-# Editar archivo de configuración de vsftpd
-sudo sed -i '$a userlist_enable=YES' /etc/vsftpd.conf
-sudo sed -i '$a userlist_file=/etc/vsftpd.user_list' /etc/vsftpd.conf
-sudo sed -i '$a userlist_deny=NO' /etc/vsftpd.conf
-sudo sed -i '$a chroot_local_user=YES' /etc/vsftpd.conf
-sudo sed -i '$a chroot_list_enable=YES' /etc/vsftpd.conf
-sudo sed -i '$a chroot_list_file=/etc/vsftpd.chroot_list' /etc/vsftpd.conf
-sudo sed -i '$a write_enable=YES' /etc/vsftpd.conf
+read -p "¿Desea editar el archivo de configuración de vsftpd? (1=Si, 2=No): " opcion
+if [ $opcion -eq 1 ]; then
+    sudo sed -i '$a userlist_enable=YES' /etc/vsftpd.conf
+    sudo sed -i '$a userlist_file=/etc/vsftpd.user_list' /etc/vsftpd.conf
+    sudo sed -i '$a userlist_deny=NO' /etc/vsftpd.conf
+    sudo sed -i '$a chroot_local_user=YES' /etc/vsftpd.conf
+    sudo sed -i '$a chroot_list_enable=YES' /etc/vsftpd.conf
+    sudo sed -i '$a chroot_list_file=/etc/vsftpd.chroot_list' /etc/vsftpd.conf
+    sudo sed -i '$a write_enable=YES' /etc/vsftpd.conf
+fi
 
 # Crear archivo chroot_list
 echo $nombre_usuario | sudo tee /etc/vsftpd.chroot_list
