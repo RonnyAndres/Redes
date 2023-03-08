be8eadcc87b89a35b426793dd67de65e6f902482
RonnyAndres <76404004+RonnyAndres@users.noreply.github.com>
2023-03-08 18:02:24 -0500

    Update punt3-lab9.sh

--- punt3-lab9.sh
+++ punt3-lab9.sh
@@ -3,23 +3,38 @@
 # Instalar vsftpd
 #sudo apt-get install vsftpd
 
+read -p "Desea crear un nuevo grupo? [1-Si] [2-No]: " crear_grupo
+
+if [ $crear_grupo -eq 1 ]; then
+  read -p "Escriba el nombre del nuevo grupo: " nombre_grupo
+  
+  # Verificar si el grupo ya existe
+  if grep -q "^$nombre_grupo:" /etc/group; then
+    echo "El grupo $nombre_grupo ya existe."
+  else
+    # Crear el grupo y agregar el usuario
+    sudo groupadd $nombre_grupo
+  fi
+fi
+
+
 # Crear nuevo usuario y establecer su contraseña
 read -p "Ingrese el nombre del nuevo usuario: " nombre_usuario
 
 # Verificar que el usuario no exista
 if id "$nombre_usuario" >/dev/null 2>&1; then
     echo "El usuario $nombre_usuario ya existe."
-    exit 1
+else
+    # Crear el usuario con su nombre como contraseña
+    sudo useradd -m -p $(openssl passwd -1 $nombre_usuario) $nombre_usuario
+    echo "El usuario $nombre_usuario se ha creado correctamente con su nombre como contraseña."
+    sudo usermod -a -G $nombre_grupo $usuario
 fi
-
-# Crear el usuario con su nombre como contraseña
-sudo useradd -m -p $(openssl passwd -1 $nombre_usuario) $nombre_usuario
-
-echo "El usuario $nombre_usuario se ha creado correctamente con su nombre como contraseña."
 
 # Crear lista de usuarios permitidos
 echo "Usuarios disponibles:"
-compgen -u | grep -v '^ftp$\|^guest\|^nobody\|root'
+compgen -u | grep -v '^ftp$\|^guest\|^nobody\|root' | grep -vFf '/etc/vsftpd.chroot_list' | awk -F':' '{if (system("id -nG "$1" | grep -qw $nombre_grupo")) print $1}'
+
 usuarios_permitidos=()
 while true; do
     read -p "Ingrese el nombre de usuario permitido (dejar en blanco para terminar): " usuario
@@ -31,7 +46,8 @@ while true; do
         usuarios_permitidos+=($usuario)
         echo "Usuarios agregados hasta el momento: ${usuarios_permitidos[*]}"
         echo "Usuarios disponibles:"
-        comm -23 <(compgen -u | grep -v '^ftp$\|^guest\|^nobody\|root' | sort) <(echo "${usuarios_permitidos[*]}" | tr ' ' '\n' | sort)
+        compgen -u | grep -v '^ftp$\|^guest\|^nobody\|root' | grep -vFf '/etc/vsftpd.chroot_list' | awk -F':' '{if (system("id -nG "$1" | grep -qw $nombre_grupo")) print $1}'
+        
     else
         echo "El usuario no existe."
     fi
@@ -50,7 +66,7 @@ if [ $opcion -eq 1 ]; then
 fi
 
 # Crear archivo chroot_list
-echo $nombre_usuario | sudo tee /etc/vsftpd.chroot_list
+echo $nombre_usuario | sudo tee -a /etc/vsftpd.chroot_list
 
 # Reiniciar vsftpd
 sudo service vsftpd restart
